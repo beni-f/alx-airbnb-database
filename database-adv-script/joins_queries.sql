dc6590a01c8a9f066fd1cf3223b2e9002fd75f74
beni-f <benifissha@gmail.com>
2024-12-01 22:22:18 +0300

    joins_queries

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,27 @@
+-- Select Database
+USE airbnb_database;
+
+-- Perfrom Inner Join
+SELECT *
+FROM Booking
+INNER JOIN User
+ON Booking.user_id = User.user_id;
+
+-- Perform Left Join
+SELECT *
+FROM Property
+LEFT JOIN Review
+ON Property.property_id = Review.property_id;
+
+-- Perfrom Full Outer Join
+SELECT *
+FROM User
+LEFT JOIN Booking
+ON User.user_id = Booking.user_id
+
+UNION
+
+SELECT *
+FROM User
+RIGHT JOIN Booking
+ON User.user_id = Booking.user_id;
