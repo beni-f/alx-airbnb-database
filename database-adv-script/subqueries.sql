6f5b6468b4b952b4b385ad2797a129b084d8acec
beni-f <benifissha@gmail.com>
2024-12-01 22:42:58 +0300

    subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,21 @@
+ -- Practice Subqueries
+
+ -- Select Database
+ USE airbnb_database;
+
+ -- A Query to find all properties where the average rating is greater than 4.0 using a subquery.
+ SELECT *
+ FROM Property
+ WHERE (
+    SELECT property_id
+    FROM Review
+    WHERE rating > 4.0
+ );
+
+ -- A correlated subquery to find users who have made more than 3 bookings
+ SELECT *
+ FROM User
+ WHERE 
+    (SELECT COUNT(*)
+    FROM Booking
+    WHERE Booking.user_id = User.user_id)  > 3;
