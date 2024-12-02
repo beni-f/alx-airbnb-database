a5c121d95c1ee0cda986dc9266cdfba200ec317d
beni-f <benifissha@gmail.com>
2024-12-02 21:25:09 +0300

    Partioning performance

---
+++ database-adv-script/partitioning.sql
@@ -0,0 +1,32 @@
+-- Select Database
+USE airbnb_database;
+
+-- Normal Table Building
+CREATE TABLE Booking (
+    booking_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36),
+    user_id CHAR(36),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Using Range Partioning
+CREATE TABLE Booking (
+    booking_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36),
+    user_id CHAR(36),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+)
+PARTITION BY RANGE(MONTH(start_date))
+PARTITIONS 5;
