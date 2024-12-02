e41e1c4456cdf9b2c04da99066504eeaf03fe779
beni-f <benifissha@gmail.com>
2024-12-02 09:06:50 +0300

    updated

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,5 @@
+-- Select Database
+USE airbnb_database;
 
 -- Index on User email (used for fast lookup)
 CREATE INDEX idx_user_email ON User(email);
@@ -20,3 +22,23 @@ CREATE INDEX idx_booking_user_id ON Booking(user_id);
 -- Index on Booking property_id (used for JOIN operations with the Property table)
 CREATE INDEX idx_booking_property_id ON Booking(property_id);
 
+-- Check the performance of a query to join User and Booking tables
+EXPLAIN ANALYZE
+SELECT U.user_id, U.first_name, B.booking_id, B.start_date
+FROM User U
+INNER JOIN Booking B ON U.user_id = B.user_id
+WHERE U.role = 'guest';
+
+-- Check the performance of a query to retrieve properties based on location
+EXPLAIN ANALYZE
+SELECT property_id, name, price_per_night
+FROM Property
+WHERE location = 'New York';
+
+-- Check the performance of a query to get the most booked properties
+EXPLAIN ANALYZE
+SELECT property_id, COUNT(*) AS booking_count
+FROM Booking
+GROUP BY property_id
+ORDER BY booking_count DESC
+LIMIT 10;
