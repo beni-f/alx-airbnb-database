25cf63ecea5102ca504bd6df527cb864f844d02e
beni-f <benifissha@gmail.com>
2024-12-02 09:39:05 +0300

    updated

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -9,13 +9,20 @@ SELECT *
 FROM Booking b
 LEFT JOIN User u ON b.user_id = u.user_id
 LEFT JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.user_id IS NOT NULL
+AND b.property_id IS NOT NULL
+AND (pay.booking_id IS NOT NULL OR pay.booking_id IS NULL);
 
 -- After analyzing it uses indexes as a reference so we can really say that performance is optimized.
+
 
 -- Refactored Query 
 SELECT *
 FROM Booking b
 LEFT JOIN User u ON b.user_id = u.user_id
 LEFT JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.user_id IS NOT NULL
+AND b.property_id IS NOT NULL
+AND (pay.booking_id IS NOT NULL OR pay.booking_id IS NULL);
